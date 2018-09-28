9062ab27ddc3bc38efa45775a60b4583c0c77110
HodaCoder <hoda.sadeghian@gmail.com>
2018-09-28 19:07:31 +0330

    Make some correction in functions

--- cnn_stock_Simple_3.py
+++ cnn_stock_Simple_3.py
@@ -177,15 +177,18 @@ x_valid /= 255
 
 # define the model
 model = Sequential()
+
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu',
                  input_shape=x_train.shape[1:]))
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+
 model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.5))
